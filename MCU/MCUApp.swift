b122e43f72ec0918c9e5693c680227e1ca4e6054
Florin Uscatu <florinusc@gmail.com>
2022-06-25 11:42:19 +0300

    Initial Commit

---
+++ MCU/MCUApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCUApp.swift
+//  MCU
+//
+//  Created by Florin Uscatu on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MCUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
